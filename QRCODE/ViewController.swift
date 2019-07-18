13d96097736021ed28887d80f90318b9b35dcc70
icenova <iceorbbroy@gmail.com>
2019-07-18 16:47:24 +0800

    Initial Commit

---
+++ QRCODE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCODE
+//
+//  Created by roy on 2019/7/18.
+//  Copyright © 2019 roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
